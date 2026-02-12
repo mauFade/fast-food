5badc3dfc2c441e06c30afc1f384023662cb42a6
Mauricio Cardoso <mauricio.cds00@gmail.com>
2026-02-12 09:26:15 -0300

    feat: add auth layout

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.mau.fastfood",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_BD_ID,
+  userCollection: process.env.EXPO_PUBLIC_USER_COLLECTION,
+};
