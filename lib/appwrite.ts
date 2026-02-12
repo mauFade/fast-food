74615017ff74543e051ce909008621bcc147c530
Mauricio Cardoso <mauricio.cds00@gmail.com>
2026-02-12 17:02:09 -0300

    feat: add backedn methods

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,58 @@
+import { CreateUserParams, SignInParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   platform: "com.mau.fastfood",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  databaseId: process.env.EXPO_PUBLIC_BD_ID,
-  userCollection: process.env.EXPO_PUBLIC_USER_COLLECTION,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+  databaseId: process.env.EXPO_PUBLIC_BD_ID!,
+  userCollection: process.env.EXPO_PUBLIC_USER_COLLECTION!,
 };
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const avatars = new Avatars(client);
+
+export const createUser = async ({
+  email,
+  name,
+  password,
+}: CreateUserParams) => {
+  try {
+    const newAccount = await account.create({
+      userId: ID.unique(),
+      name,
+      email,
+      password,
+    });
+
+    if (!newAccount) throw Error;
+
+    await signIn({ email, password });
+
+    const avatarUrl = avatars.getInitialsURL(name);
+
+    return await databases.createDocument({
+      databaseId: appwriteConfig.databaseId,
+      collectionId: appwriteConfig.userCollection,
+      documentId: ID.unique(),
+      data: {
+        accountId: newAccount.$id,
+        email,
+        name,
+        avatar: avatarUrl,
+      },
+    });
+  } catch (error) {
+    throw new Error(JSON.stringify(error));
+  }
+};
+
+export const signIn = async ({ email, password }: SignInParams) => {};
